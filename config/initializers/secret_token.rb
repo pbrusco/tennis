fecb5f2144f34fe5978af7040adb03da5c95395b
pablo <pablo.brusco@gmail.com>
2012-09-26 19:02:48 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tennis::Application.config.secret_token = '882b4b924d243750eb8614e1f5e3d935759eb6d1b98897318e31853b1b8feb63b3caf485669f4cfc04e807724298441e0ea38f4b13907dfd67c4b84b6c012d1f'
